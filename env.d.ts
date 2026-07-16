1fc0e2e632f6181dec784e140cd67efc95ba06d9
grind-t <grindt.gd@gmail.com>
2026-07-16 17:44:17 +0400

    upd

--- env.d.ts
+++ env.d.ts
@@ -4,6 +4,7 @@ declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			T_INVEST_READONLY_TOKEN: string;
+			T_INVEST_ACCOUNT_ID: string
 		}
 	}
 }
--- src/bonds/pull.ts
+++ src/bonds/pull.ts
@@ -5,15 +5,31 @@ import { env } from "node:process";
 import { DuckDBInstance } from "@duckdb/node-api";
 import { listAllRussianBonds } from "@grind-t/russian-bonds";
 import { flatten } from "flat";
+import { TInvestApi, tInvestNumber } from "@grind-t/t-invest";
 
-const bonds = await listAllRussianBonds(env.T_INVEST_READONLY_TOKEN);
-const flatBonds = bonds.map((bond) => flatten(bond, { delimiter: "_" }));
+const tInvestApi = new TInvestApi(env.T_INVEST_READONLY_TOKEN)
 const tmpFile = join(tmpdir(), `bonds-${Date.now()}.json`);
-
-writeFileSync(tmpFile, JSON.stringify(flatBonds));
-
-const db = await DuckDBInstance.create();
-const conn = await db.connect();
 const dataFile = join(import.meta.dirname, "data.parquet");
 
-await conn.run(/*sql*/ `COPY '${tmpFile}'TO '${dataFile}'`);
+const [db, portfolio, bonds] = await Promise.all([
+  DuckDBInstance.create().then(v => v.connect()),
+  tInvestApi.operations.getPortfolio({accountId: env.T_INVEST_ACCOUNT_ID}),
+  listAllRussianBonds(env.T_INVEST_READONLY_TOKEN), 
+]);
+
+const data = bonds.map((bond) => {
+  const position = portfolio.positions.find((v) => v.ticker === bond.isin)
+  const flattendBond = flatten(bond, { delimiter: "_" }) as any
+
+  if (position) {
+    const quantity = position.quantity ? tInvestNumber(position.quantity) : 0
+    const avgPrice = position.averagePositionPrice ? tInvestNumber(position.averagePositionPrice) : 0
+    flattendBond.total_invested = quantity * avgPrice
+  }
+
+  return flattendBond
+})
+
+writeFileSync(tmpFile, JSON.stringify(data));
+
+await db.run(/*sql*/ `COPY '${tmpFile}'TO '${dataFile}'`);
